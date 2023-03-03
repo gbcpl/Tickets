70c40e10939470c6da639807a98f5c7a0aa26bd3
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-03-03 19:08:44 +0100

    Trying express static

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const app = express();              //Instantiate an express app, the main work 
 const port = 8000;                  //Save the port number where your server will be listening
 const hostname = '127.0.0.1';
 
+app.use(express.static('app/views/'));
+
 const path = __dirname + '/app/views/public/';
 app.use(express.static(path));
+
 
 app.get('/', function (req,res) {
     res.sendFile(path + "index.html");
   });
-  
+
+
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on http://${hostname}:${port}/`);
 });
+
