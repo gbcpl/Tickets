9e8f55ace64623a4b670913983a7593ce2985936
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-03-03 13:22:01 +0100

    Views added but doesn't work

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+const port = 8000;                  //Save the port number where your server will be listening
 const hostname = '127.0.0.1';
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+const path = __dirname + '/app/views/public/';
+app.use(express.static(path));
 
-app.get('/categories', (req, res) => {       
-    res.sendFile('categories.html', {root: __dirname});      
-                                                       
-});
-
-app.get('/closedtickets', (req, res) => {       
-    res.sendFile('closedtickets.html', {root: __dirname});      
-                                                       
-});
-
-app.get('/tickets', (req, res) => {       
-    res.sendFile('tickets.html', {root: __dirname});      
-                                                       
-});
-
+app.get('/', function (req,res) {
+    res.sendFile(path + "index.html");
+  });
+  
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on http://${hostname}:${port}/`);
 });
