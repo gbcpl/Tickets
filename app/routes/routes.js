9c542da7ae2b678443e3a2af1b7e22f5ab7767a9
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-03-10 19:38:41 +0100

    Routes API

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const sequelize = require('../config/database-config')
+const { QueryTypes } = require('sequelize');
 
-router.get('/:id', async function(req, res) {
+
+router.get('/:id', async function(req,res){
+    console.log(req.params.id)
+
     try {
-        const sqlQuery = 'SELECT id, author FROM tickets WHERE id=?';
-        const rows = await sequelize.query(sqlQuery, req.params.id);
-        res.status(200).json(rows); 
+        const sqlQuery = 'SELECT id, author, description FROM tickets WHERE id = ?';
+        const rows = await sequelize.query(sqlQuery, {
+            replacements: [req.params.id],
+            type: QueryTypes.SELECT
+          });
+        res.status(200).json(rows);
     } catch (error) {
         res.status(400).send(error.message)
     }
-    res.status(200).json({id:req.params.id})
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 8000;                  //Save the port number where your server will be listening
 const hostname = '127.0.0.1';
-const path = require('path');
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+// const path = require('path');
+const ticketRouter = require('./app/routes/routes');
+app.use('/tickets', ticketRouter);
 
 const index = __dirname + '/app/views/';
 app.use(express.static(index));
@@ -14,8 +14,6 @@ app.get('/', function (req,res) {
   res.sendFile(index + "index.html");
 });
 
-const ticketRouter = require('./app/routes/routes');
-app.use('/tickets', ticketRouter);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on http://${hostname}:${port}/`);
