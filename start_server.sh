4081c03cef596c25261c1fca25028f7676d64583
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2023-03-13 13:12:26 +0100

    Set pass automatically with script .sh

--- start_server.sh
+++ start_server.sh
@@ -3,3 +3,15 @@ apt install mariadb-server -y
 
 echo "DOSN'T FORGET TO PROTECT THE MYSQL CONNECTION AND SERVER"
 echo "You can use : mysql_secure_installation"
+
+service mysql restart
+
+mysql -u root
+
+use mysql;
+
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('94dy8vURzDEF78DHZ8DZ8712DKz3Nh7');
+
+flush privileges;
+
+quit;
