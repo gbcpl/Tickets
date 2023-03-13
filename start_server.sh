575ed3dc3b2f29b223732acdce7530095dc7bf04
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2023-03-13 13:17:56 +0100

    set start Deploy for package.json and set call on start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -12,6 +12,10 @@ use mysql;
 
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('94dy8vURzDEF78DHZ8DZ8712DKz3Nh7');
 
+CREATE DATABASE IF NOT EXISTS tickets;
+
 flush privileges;
 
 quit;
+
+npm run startdeploy
