43ac0bc5645affe7d3a83f7afee598d8b889bbeb
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-03-16 16:21:49 +0100

    Routes modified

--- app/config/database-config.js
+++ app/config/database-config.js
@@ -5,6 +5,7 @@ const sqlDB = process.env.SQLDB;
 const sqlUser = process.env.SQLUSER;
 const sqlPassword = process.env.SQLPASS;
 const sqlHost = process.env.SQLHOST;
+
 console.log({
     db : sqlDB,
     user: sqlUser,
--- app/routes/ticket.js
+++ app/routes/ticket.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// const app = express();  
 const { QueryTypes } = require('sequelize');
 const Tickets = require('../models/index.js');
 const sequelize = require('../config/database-config')
 
 router.get('/count', async (req, res) => {
     try {
-      const count = await Tickets.count();
-      console.log(count)
-      res.json({ count });
+      const sqlQuery = 'SELECT COUNT(*) FROM tickets'
+      const count = await sequelize.query(sqlQuery, {
+        type: QueryTypes.SELECT
+      })
+      res.status(200).json({ count });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Erreur lors du comptage des tickets.' });
@@ -40,6 +41,24 @@ router.get('/tags', async function(req,res){
   }
 });
 
+router.post('/register', async (req, res) => {
+    try 
+        {        
+        const sqlQuery = 'INSERT INTO tickets (title, description, category) VALUES (:title, :description, :category)'
+        const result = await sequelize.query(sqlQuery, {
+          replacements: {
+              title: req.body.title,
+              description: req.body.description,
+              category: Number(req.body.category)
+          },
+          type: QueryTypes.INSERT
+      });
+
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
 
 router.get('/:id', async function(req,res){
     try {
@@ -54,20 +73,7 @@ router.get('/:id', async function(req,res){
     }
 });
 
-router.post('/register', async (req, res) => {
-    try 
-        {        
-        const result = await Tickets.create({
-            title: req.body.title,
-            description: req.body.description,
-            category: Number(req.body.category)
-        });
 
-        res.status(200).json(result);
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-});
 
 
 
