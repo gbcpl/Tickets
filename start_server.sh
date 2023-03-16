0278aea0a7979b236b4d2e3888080f2ebb55024c
SlyderSlash <39804182+SlyderSlash@users.noreply.github.com>
2023-03-16 11:50:35 +0100

    set connexion & pass for sql server

--- start_server.sh
+++ start_server.sh
@@ -1,21 +1,9 @@
-apt update
-apt install mariadb-server -y
+apt update & apt install mariadb-server -y
 
-echo "DOSN'T FORGET TO PROTECT THE MYSQL CONNECTION AND SERVER"
 echo "You can use : mysql_secure_installation"
 
 service mysql restart
 
-mysql -u root
-
-use mysql;
-
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('94dy8vURzDEF78DHZ8DZ8712DKz3Nh7');
-
-CREATE DATABASE IF NOT EXISTS tickets;
-
-flush privileges;
-
-quit;
+mysql -u root < "startServer.sql"
 
 npm run startdeploy
