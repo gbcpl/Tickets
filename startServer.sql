c4c59f80732668c10df90c4014c772222d36cbf6
Gabriel CAPELL <capell.gabriel@gmail.com>
2023-03-20 15:30:56 +0100

    SQL file updated because of errors

--- startServer.sql
+++ startServer.sql
@@ -4,57 +4,58 @@ SET PASSWORD FOR 'root'@'localhost' = PASSWORD('94dy8vURzDEF78DHZ8DZ8712DKz3Nh7'
 
 CREATE DATABASE IF NOT EXISTS tickets;
 
+USE tickets;
+
+CREATE TABLE categories
+(
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    cname VARCHAR(60) NOT NULL,
+    idDiscord VARCHAR(80),
+    parent INT
+);
+
 CREATE TABLE tickets
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     author INT NOT NULL,
-    category INT NOT NULL,
+    category VARCHAR(500) NOT NULL,
     title VARCHAR(60) NOT NULL,
-    description VARCHAR(1000) NOT NULL,
+    tdescription VARCHAR(1000) NOT NULL,
     screenshot BLOB,
-    subcategory INT,
     createdDate DATETIME,
     closingDate DATETIME
-    FOREIGN KEY (category) REFERENCES categories(id)
-)
+);
 
 CREATE TABLE logtickets
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     author INT NOT NULL,
-    message VARCHAR(1000) NOT NULL,
+    tmessage VARCHAR(1000) NOT NULL,
     ticketId INT,
-    date DATETIME,
-    state VARCHAR(60),
+    tdate DATETIME,
+    tstate VARCHAR(60),
     FOREIGN KEY (ticketId) REFERENCES tickets(id)
-)
-
-CREATE TABLE categories
-(
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name VARCHAR(60) NOT NULL,
-    parent INT,
-)
+);
 
 CREATE TABLE login
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    idDiscord CHAR,
+    idDiscord CHAR(18),
     username VARCHAR(60),
     profilePicture VARCHAR(255),
-    logID CHAR,
-    passID CHAR,
+    logID INT(4),
+    passID CHAR(24),
     expiration BIGINT,
-    active BOOLEAN
-)
+    active BOOLEAN DEFAULT true
+);
 
 CREATE TABLE token
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    token VARCHAR(100) NOT NULL,
+    token VARCHAR(300) NOT NULL,
     expire DATETIME NOT NULL,
-    loginfo PRIMARY KEY NOT NULL,
+    loginfo INT UNIQUE NOT NULL,
     FOREIGN KEY (loginfo) REFERENCES login(id)
-)
+);
 
 flush privileges;
